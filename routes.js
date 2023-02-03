8408741583007bb30d47368c831e7c29813cde14
Mathis Lambert <mathis.lambert27@gmail.com>
2023-02-03 16:37:08 +0100

    https redirection

--- routes.js
+++ routes.js
@@ -21,6 +21,12 @@ module.exports = (app) => {
   ///////////////////////////////////////////////////////////
   app.get("/", (req, res, next) => {
     res.sendFile(path.join(public, "/views/index.html"));
+    // redirect to https
+    if (req.headers["x-forwarded-proto"] !== "https") {
+      res.redirect("https://" + req.headers.host + req.url);
+    } else {
+      next();
+    }
   });
 
   app.post("/", (req, res, next) => {
